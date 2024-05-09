7d75e00b4d91e3e49e75575e7825456f73f9371c
Diyo Anggara <xdamazon17@gmail.com>
2024-05-09 19:52:44 +0700

    feat: :sparkles: awesome trash
    add trash box feature, archive, removing note, and reusable modal confirm

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
+import { ChevronsLeft, Inbox, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 
@@ -12,6 +12,8 @@ import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { TrashBox } from "./trash-box";
 
 
 const Navigation = () => {
@@ -134,20 +136,44 @@ const Navigation = () => {
              isSearch
              onClick={() => {}}
             />
+             <Item
+             label="Inbox"
+             icon={Inbox}
+             onClick={() => {}}
+             
+             />
             <Item
              label="Settings"
              icon={Settings}
              onClick={() => {}}
             />
-            <Item
+            {/* <Item
               onClick= {handleCreate}
               label="New page"
               icon={PlusCircle} 
             
-            />
+            /> */}
         </div>
         <div className="mt-4">
             <DocumentList/>
+            <Item
+             onClick={handleCreate}
+             label="Add a page"
+             icon={Plus}
+
+            />
+            <Popover>
+                <PopoverTrigger className="w-full mt-4">
+                    <Item 
+                    label="Trash"
+                    icon={Trash}
+                    
+                    />
+                </PopoverTrigger>
+                <PopoverContent className="p-0 w-72" side={isMobile ? "bottom" : "right"}>
+                    <TrashBox/>
+                </PopoverContent>
+            </Popover>
         </div>
         <div 
         onMouseDown={handleMouseDown}
--- convex/documents.ts
+++ convex/documents.ts
@@ -15,7 +15,7 @@ import { Doc, Id } from "./_generated/dataModel";
 
 //     return documents;
 
-//   },
+//   }, 
 // });
 
 export const archive = mutation({
@@ -24,7 +24,7 @@ export const archive = mutation({
         const identity = await ctx.auth.getUserIdentity();
 
         if (!identity) {
-          throw new Error("You must be logged in to create a document");
+          throw new Error("Not authenticated");
         }
     
         const userId = identity.subject;
@@ -61,6 +61,8 @@ export const archive = mutation({
             isArchived: true,
         });
 
+        recursiveArchive(args.id);
+
         return document;
     }
 })
@@ -73,7 +75,7 @@ export const getSidebar = query({
     const identity = await ctx.auth.getUserIdentity();
 
     if (!identity) {
-      throw new Error("You must be logged in to create a document");
+      throw new Error("Not authenticated");
     }
 
     const userId = identity.subject;
@@ -101,7 +103,7 @@ export const create = mutation({
     const identity = await ctx.auth.getUserIdentity();
 
     if (!identity) {
-      throw new Error("You must be logged in to create a document");
+      throw new Error("Not authenticated");
     }
 
     const userId = identity.subject;
@@ -116,3 +118,104 @@ export const create = mutation({
     return document;
   },
 });
+
+export const getTrash = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user", (query) => query.eq("userId", userId)).filter((query) => query.eq(query.field("isArchived"), true)).order("desc").collect();
+
+      return documents;
+  }
+});
+
+export const restore = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if(!existingDocument) {
+      throw new Error("Not found");
+    }
+    if(existingDocument.userId !== userId){
+      throw new Error("Unauthorized");
+    }
+
+    const recuriveRestore = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+       .query("documents")
+       .withIndex("by_user_parent", (query) =>
+          query.eq("userId", userId).eq("parentDocument", documentId)
+        )
+       .collect();
+
+       for (const child of children){
+          await ctx.db.patch(child._id, {
+            isArchived: false,
+          });
+
+          await recuriveRestore(child._id)
+       }
+    }
+    
+    const options: Partial<Doc<"documents">> = {
+      isArchived: false,
+    }
+
+    if(existingDocument.parentDocument){
+      const parent = await ctx.db.get(existingDocument.parentDocument)
+      if(parent?.isArchived){
+        options.parentDocument = undefined
+      }
+    }
+
+    const document = await ctx.db.patch(args.id, options);
+
+    recuriveRestore(args.id)
+
+    return document;
+  }
+});
+
+export const remove = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if(!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if(existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.delete(args.id);
+
+    return document;
+  }
+})
