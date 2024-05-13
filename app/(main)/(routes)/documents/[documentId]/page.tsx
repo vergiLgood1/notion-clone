f4a594263e8e573a3d4879042bfbd3185eddbb4f
Diyo Anggara <xdamazon17@gmail.com>
2024-05-13 17:24:45 +0700

    feat: adding and configuring a component editor using blocknote react

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { Cover } from "@/components/cover";
+import { Editor } from "@/components/editor";
 import { Toolbar } from "@/components/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 interface DocumentIdPageProps {
     params: {
@@ -19,6 +20,15 @@ const DocumentIdPage = ({
     const document = useQuery(api.documents.getById, {
         documentId: params.documentId
     });
+
+    const update = useMutation(api.documents.update);
+
+    const onChange = (content: string) => {
+        update({
+            id: params.documentId,
+            content
+        });
+    };
 
     if (document === undefined) {
         return (
@@ -49,7 +59,11 @@ const DocumentIdPage = ({
             <Cover url={document.coverImage} />
             <div className=" md:max-w-3xl lg:max-w-4xl mx-auto ">
                 <Toolbar initialData={document} />
-
+                <Editor
+                    onChange={onChange}
+                    initialContent={document.content}
+                
+                />
             </div>
         </div>
      );
---
+++ components/editor.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/core/fonts/inter.css";
+import "@blocknote/mantine/style.css";
+import { useTheme } from "next-themes";
+import { useEdgeStore } from "@/lib/edgestore";
+import { PartialBlock } from "@blocknote/core";
+
+
+interface EditorProps {
+    onChange: (value: string) => void;
+    initialContent?: string;
+    editable?: boolean;
+};
+
+export const Editor = ({
+    onChange,
+    initialContent,
+    editable,
+}: EditorProps) => {
+    const { resolvedTheme } = useTheme();
+    const { edgestore } = useEdgeStore();
+
+    const handleUpload = async (file: File) => {
+        const response = await edgestore.publicFiles.upload({
+            file
+        })
+
+        return response.url;
+    };
+
+    
+    const onEditorContentChange = (editor: any) => {
+        onChange(JSON.stringify(editor.topLevelBlcoks, null, 2))
+    }
+
+    const editor = useCreateBlockNote({
+    initialContent: 
+        initialContent 
+        ? JSON.parse(initialContent) as PartialBlock[] 
+        : undefined,
+    uploadFile:handleUpload,    
+  });
+
+    return(
+        <div>
+            <BlockNoteView
+               editor={editor}
+               theme={resolvedTheme === "dark" ? "dark" : "light"}
+               onChange={() => {
+                onChange(JSON.stringify(editor.document, null, 2))
+               }}
+            />
+        </div>
+    )
+}
