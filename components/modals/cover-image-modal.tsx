f3843dba83bc98beb6c530bb9d2c976539f6134b
Diyo Anggara <xdamazon17@gmail.com>
2024-05-13 15:32:42 +0700

    feat: improve code to make an advanced upload file, Replcae, and deleted file in edgestore

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -32,7 +32,10 @@ export const CoverImageModal = () => {
             setFile(file);
 
             const res = await edgestore.publicFiles.upload({
-                file
+                file,
+                options: {
+                    replaceTargetUrl: coverImage.url
+                }
             });
 
             await update({
